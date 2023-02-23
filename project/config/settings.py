ebc827b5faa09a88b26c476565f27850e2034022
keannu125 <noehbernasol0@gmail.com>
2023-02-24 00:39:36 +0800

    Revert hiding secret key so you can clone repo directly

--- project/config/settings.py
+++ project/config/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-=p&04v6!#hvz*g1ht4c7tjcsy6*e61u7tkv8ird9b*#%lo$kjx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
