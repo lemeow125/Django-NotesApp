4ad3bedb18744150ab943fdd6a870c9d6a7a772d
keannu125 <noehbernasol0@gmail.com>
2023-02-25 17:57:38 +0800

    Added djoser

--- project/config/settings.py
+++ project/config/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'notes.apps.NotesConfig',
-    'corsheaders'
+    'corsheaders',
+    'djoser'
 ]
 
 MIDDLEWARE = [
