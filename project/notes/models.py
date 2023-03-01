65ad4a5f21599efb1f199878b7ae4927e57e9e7c
keannu125 <noehbernasol0@gmail.com>
2023-03-01 22:58:22 +0800

    Notes now has owner foreign key

--- project/notes/models.py
+++ project/notes/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils.timezone import now
+from django.contrib.auth import get_user_model
+User = get_user_model()
 # Create your models here.
 
 
@@ -7,6 +9,7 @@ class Note(models.Model):
     title = models.CharField(max_length=20)
     content = models.CharField(max_length=1024)
     date_created = models.DateTimeField(default=now, editable=False)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
