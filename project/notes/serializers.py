ec74dbdaf5aae871ccbaa34d4487d41d7e2c1553
keannu125 <noehbernasol0@gmail.com>
2023-03-04 19:25:40 +0800

    Hotfix for not being able to POST with changed 12 hour datetime format

--- project/notes/serializers.py
+++ project/notes/serializers.py
@@ -4,8 +4,10 @@ from .models import Note
 
 class NoteSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-    date_created = serializers.DateTimeField(format="%d-%m-%Y %I:%M%p")
+    date_created = serializers.DateTimeField(
+        format="%d-%m-%Y %I:%M%p", read_only=True)
 
     class Meta:
         model = Note
         fields = ('id', 'title', 'content', 'date_created', 'owner')
+        read_only_fields = ('id', 'date_created', 'owner')
