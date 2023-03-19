d08be3754f978bebd98eae1fda8470fad5148e43
keannu125 <noehbernasol0@gmail.com>
2023-03-19 20:56:50 +0800

    Added allowed hosts

--- project/config/settings.py
+++ project/config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lemeow125.github.io', 'keannu126.pythonanywhere.com']
 
 
 # Application definition
